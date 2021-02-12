cb56181d35ecb34da861fb5c0c2ad99d2eb7edcc
Erin Sheldon <erin.sheldon@gmail.com>
2021-02-12 11:28:03 -0500

    add license and bump version to 0.9.0

--- kmeans_radec/__init__.py
+++ kmeans_radec/__init__.py
@@ -1,3 +1,5 @@
+__version == '0.9.0'
+
 from . import kmeans_radec
 from .kmeans_radec import KMeans, kmeans, kmeans_sample
 from .kmeans_radec import find_nearest, cdist_radec 
--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
-setup(name="kmeans_radec", 
-      url="https://github.com/esheldon/kmeans_radec",
-      description="perform kmeans clustering on the unit sphere",
-      packages=['kmeans_radec'],
-      author="Erin Scott Sheldon",
-      author_email="erin.sheldon@gmail.com",
-      install_requires=['numpy'],
-      version="0.1")
+setup(
+    name="kmeans_radec", 
+    url="https://github.com/esheldon/kmeans_radec",
+    description="perform kmeans clustering on the unit sphere",
+    packages=['kmeans_radec'],
+    author="Erin Scott Sheldon",
+    author_email="erin.sheldon@gmail.com",
+    install_requires=['numpy'],
+    version="0.9.0",
+)
